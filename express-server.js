142d208afa9cc819f061f619cbd9045d1bb4a5ed
roshanpawar17 <roshanpawar119@gmail.com>
2024-07-08 00:35:04 +0530

    remove static files extension

--- express-server.js
+++ express-server.js
@@ -30,20 +30,20 @@ const app = express();
 
 // })
 
-const publicPath = path.join(__dirname, "public");
-app.use(express.static(publicPath));
+// const publicPath = path.join(__dirname, "public");
+// app.use(express.static(publicPath));
 
-// app.get('', (req, res)=>{
-//     res.sendFile(`${publicPath}/index.html`)
-// });
+app.get('', (req, res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+});
 
-// app.get('/about', (req, res)=>{
-//     res.sendFile(`${publicPath}/about.html`)
-// });
+app.get('/about', (req, res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+});
 
-// app.get('*', (req, res)=>{
-//     res.sendFile(`${publicPath}/not-found.html`)
-// });
+app.get('*', (req, res)=>{
+    res.sendFile(`${publicPath}/not-found.html`)
+});
 
 
 
