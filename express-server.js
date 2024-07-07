27e3f4c231961cb2a04ebf640bc9a27aa8ecd66c
roshanpawar17 <roshanpawar119@gmail.com>
2024-07-08 00:33:51 +0530

    simple api, json response, link pages

--- express-server.js
+++ express-server.js
@@ -3,32 +3,32 @@ const path = require('path');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-// app.get('', (req, res)=>{
-//     res.send(`
-//         <h1>Hello World</h1>
-//         <a href="about">About</a>
-//     `);
-    // res.send([
-    //     {
-    //         name: 'John',
-    //         age: 30
-    //     },
-    //     {
-    //         name: 'Roshan',
-    //         age: 21
-    //     },
-    // ]);
-// })
+app.get('', (req, res)=>{
+    res.send(`
+        <h1>Hello World</h1>
+        <a href="about">About</a>
+    `);
+    res.send([
+        {
+            name: 'John',
+            age: 30
+        },
+        {
+            name: 'Roshan',
+            age: 21
+        },
+    ]);
+})
 
-// app.get('/about', (req, res)=>{
-//     res.send(`
-//         <h1>About Page</h1>
-//         <a href="/">Home</a>
-//     `);
+app.get('/about', (req, res)=>{
+    res.send(`
+        <h1>About Page</h1>
+        <a href="/">Home</a>
+    `);
 
-// })
+})
 
 // const publicPath = path.join(__dirname, "public");
 // app.use(express.static(publicPath));
@@ -48,10 +48,10 @@ app.set('view engine', 'ejs');
 
 
 
-app.get('', (req, res)=>{
-    const user = { name: "Roshan", skills: ['Html', 'css', 'js'] }
-    res.render('index', {user});
-});
+// app.get('', (req, res)=>{
+//     const user = { name: "Roshan", skills: ['Html', 'css', 'js'] }
+//     res.render('index', {user});
+// });
  
 
 app.listen(80); 
